a46c709a67356601491dfbecbf40393bcd62356b
thibautpierron <tpierron@student.42.fr>
2018-04-21 14:22:36 +0200

    add movable

---
+++ Assets/Scripts/Movable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movable : MonoBehaviour
+{
+  private NavMeshAgent agent;
+  // Use this for initialization
+  void Start()
+  {
+    agent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+
+      if (Physics.Raycast(ray, out hit))
+      {
+        agent.SetDestination(hit.point);
+      }
+
+    }
+  }
+}
