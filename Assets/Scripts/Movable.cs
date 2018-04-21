86c59f9eaf0dc27bd4df3038f9f67d29b6ec9d9e
thibautpierron <tpierron@student.42.fr>
2018-04-21 17:16:21 +0200

    moving animation done

--- Assets/Scripts/Movable.cs
+++ Assets/Scripts/Movable.cs
@@ -5,14 +5,47 @@ using UnityEngine.AI;
 
 public class Movable : MonoBehaviour
 {
+  public float notMovingTreshold = 0.1f;
   private NavMeshAgent agent;
+  private Animator animator;
   void Start()
   {
     agent = GetComponent<NavMeshAgent>();
+    animator = GetComponentInChildren<Animator>();
+  }
+
+  void Update()
+  {
+    transform.forward = Vector3.forward;
+    CheckMovement();
   }
 
   public void MoveTo(Vector3 position)
   {
     agent.SetDestination(position);
+    Vector3 dir = transform.position - position;
+    animator.SetBool("IsMoving", true);
+    SetFacingDirection(dir);
+  }
+
+  private void CheckMovement()
+  {
+    float velocity = agent.velocity.magnitude;
+    if (velocity < notMovingTreshold)
+      animator.SetBool("IsMoving", false);
+  }
+
+  private void SetFacingDirection(Vector3 dir)
+  {
+    float angle = Vector3.SignedAngle(dir, Vector3.forward, Vector3.up);
+    Debug.Log(angle);
+    if (angle > -135 && angle < -45)
+      animator.SetInteger("FacingDirection", 0);
+    else if (angle > -45 && angle < 45)
+      animator.SetInteger("FacingDirection", 1);
+    else if (angle > 45 && angle < 135)
+      animator.SetInteger("FacingDirection", 2);
+    else
+      animator.SetInteger("FacingDirection", 3);
   }
 }
