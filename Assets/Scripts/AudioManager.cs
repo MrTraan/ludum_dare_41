613d01425ef4d439056c0f697a4ca383edda4c0c
thibautpierron <ngrasset@student.42.fr>
2018-04-21 11:09:57 +0200

    add singletons

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+  public static AudioManager instance { get; private set; }
+
+  void Awake()
+  {
+    instance = this;
+  }
+}
