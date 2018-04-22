b963989fbd9c0be598c873df7a1daf5808cbaaac
Nathan Grasset <ngrasset@student.42.fr>
2018-04-22 11:11:50 +0200

    reorganize scprits

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -11,14 +11,17 @@ public enum eResource
 
 public class ResourceManager : MonoBehaviour
 {
+	public int startingGold = 0;
+	public int startingVegetables = 0;
+	public int startingMeat = 0;
 	[SerializeField]
 	private Dictionary<eResource, int> stock = new Dictionary<eResource, int>();
 
 	private void Start()
 	{
-		stock[eResource.GOLD] = 0;
-		stock[eResource.VEGETABLES] = 0;
-		stock[eResource.MEAT] = 1000;
+		stock[eResource.GOLD] = startingGold;
+		stock[eResource.VEGETABLES] = startingVegetables;
+		stock[eResource.MEAT] = startingMeat;
 		
 	}
 
