e01805f1714416adfad3962778215c4647560d0d
thibautpierron <tpierron@student.42.fr>
2018-04-22 14:35:12 +0200

    ui organisation

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,62 +7,64 @@ using UnityEngine;
 [RequireComponent(typeof(RecipeManager))]
 public class GameManager : MonoBehaviour
 {
-	public static GameManager instance { get; private set; }
+  public static GameManager instance { get; private set; }
 
-	public static SelectionManager selectionManager { get; private set; }
+  public static SelectionManager selectionManager { get; private set; }
 
-	public static ResourceManager resourceManager { get; private set; }
+  public static ResourceManager resourceManager { get; private set; }
 
-	public static RecipeManager recipeManager { get; private set; }
+  public static RecipeManager recipeManager { get; private set; }
+  public static UIManager uiManager { get; private set; }
 
-	void Awake()
-	{
-		instance = this;
-		selectionManager = GetComponent<SelectionManager>();
-		resourceManager = GetComponent<ResourceManager>();
-		recipeManager = GetComponent<RecipeManager>();
-	}
+  void Awake()
+  {
+    instance = this;
+    selectionManager = GetComponent<SelectionManager>();
+    resourceManager = GetComponent<ResourceManager>();
+    recipeManager = GetComponent<RecipeManager>();
+    uiManager = GetComponent<UIManager>();
+  }
 
-	private void Update()
-	{
-		if (Input.GetMouseButtonUp(1))
-		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			Order o = new Order();
-			if (Physics.Raycast(ray, out hit))
-			{
-				if (hit.transform.tag == "Station")
-				{
-					o.type = eOrderType.WORK;
-					o.position = hit.point;
-					o.station = hit.transform.gameObject.GetComponent<IStation>();
-					selectionManager.DispatchOrder(o);
-				}
-				else if (hit.transform.tag == "Truck")
-				{
-					if (hit.transform.gameObject.GetComponent<Truck>().state == Truck.eState.UNLOADING)
-					{
-						o.type = eOrderType.WORK;
-						o.position = hit.point;
-						o.station = hit.transform.gameObject.GetComponent<IStation>();
-						selectionManager.DispatchOrder(o);
-					}
-					else
-					{
-						o.type = eOrderType.MOVE;
-						o.position = hit.point;
-						selectionManager.DispatchOrder(o);
-					}
-				}
-				else
-				{
-					o.type = eOrderType.MOVE;
-					o.position = hit.point;
-					selectionManager.DispatchOrder(o);
-				}
-			}
-		}
-	}
+  private void Update()
+  {
+    if (Input.GetMouseButtonUp(1))
+    {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      Order o = new Order();
+      if (Physics.Raycast(ray, out hit))
+      {
+        if (hit.transform.tag == "Station")
+        {
+          o.type = eOrderType.WORK;
+          o.position = hit.point;
+          o.station = hit.transform.gameObject.GetComponent<IStation>();
+          selectionManager.DispatchOrder(o);
+        }
+        else if (hit.transform.tag == "Truck")
+        {
+          if (hit.transform.gameObject.GetComponent<Truck>().state == Truck.eState.UNLOADING)
+          {
+            o.type = eOrderType.WORK;
+            o.position = hit.point;
+            o.station = hit.transform.gameObject.GetComponent<IStation>();
+            selectionManager.DispatchOrder(o);
+          }
+          else
+          {
+            o.type = eOrderType.MOVE;
+            o.position = hit.point;
+            selectionManager.DispatchOrder(o);
+          }
+        }
+        else
+        {
+          o.type = eOrderType.MOVE;
+          o.position = hit.point;
+          selectionManager.DispatchOrder(o);
+        }
+      }
+    }
+  }
 
 }
