409288a4f5c873c37bc7cf07dfc9432d759ba712
thibautpierron <tpierron@student.42.fr>
2018-04-22 20:12:33 +0200

    change outline to sprite

--- Assets/Materials/SpriteOutline.cs
+++ Assets/Materials/SpriteOutline.cs
@@ -31,11 +31,12 @@ public class SpriteOutline : MonoBehaviour
 
   void UpdateOutline(bool outline)
   {
-    MaterialPropertyBlock mpb = new MaterialPropertyBlock();
-    spriteRenderer.GetPropertyBlock(mpb);
-    mpb.SetFloat("_Outline", outline ? 1f : 0);
-    mpb.SetColor("_OutlineColor", color);
-    mpb.SetFloat("_OutlineSize", outlineSize);
-    spriteRenderer.SetPropertyBlock(mpb);
+    // MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+    // spriteRenderer.GetPropertyBlock(mpb);
+    // mpb.SetFloat("_Outline", outline ? 1f : 0);
+    // mpb.SetColor("_OutlineColor", color);
+    // mpb.SetFloat("_OutlineSize", outlineSize);
+    // spriteRenderer.SetPropertyBlock(mpb);
+    spriteRenderer.enabled = outline;
   }
 }
