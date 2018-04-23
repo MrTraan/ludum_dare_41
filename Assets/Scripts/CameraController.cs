7964a3f2796f7fecd6eefa2186163030f816dcac
thibautpierron <tpierron@student.42.fr>
2018-04-23 11:21:12 +0200

    update navmesh & camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,23 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-	public float panSpeed = 20.0f;
-	public float panBorderThickness = 10.0f;
+public class CameraController : MonoBehaviour
+{
+  public float panSpeed = 20.0f;
+  public float panBorderThickness = 10.0f;
 
-	void Update () {
-		Vector3 pos = transform.position;
+  public float leftBound = 0;
+  public float rightBound = 0;
+  public float topBound = 0;
+  public float bottomBound = 0;
 
-		if (Input.mousePosition.z >= Screen.height - panBorderThickness)
-			pos.y += panSpeed * Time.deltaTime;
-		if (Input.mousePosition.y <= panBorderThickness)
-			pos.y -= panSpeed * Time.deltaTime;
-		if (Input.mousePosition.x >= Screen.width - panBorderThickness)
-			pos.x += panSpeed * Time.deltaTime;
-		if (Input.mousePosition.x <= panBorderThickness)
-			pos.x -= panSpeed * Time.deltaTime;
+  void Update()
+  {
+    Vector3 pos = transform.position;
 
-		transform.position = pos;
-		
-	}
+
+
+    if (Input.mousePosition.y >= Screen.height - panBorderThickness && pos.z < topBound)
+      pos.z += panSpeed * Time.deltaTime;
+    if (Input.mousePosition.y <= panBorderThickness && pos.z > bottomBound)
+      pos.z -= panSpeed * Time.deltaTime;
+    if (Input.mousePosition.x >= Screen.width - panBorderThickness && pos.x < rightBound)
+      pos.x += panSpeed * Time.deltaTime;
+    if (Input.mousePosition.x <= panBorderThickness && pos.x > leftBound)
+      pos.x -= panSpeed * Time.deltaTime;
+
+    transform.position = pos;
+
+  }
+
 }
