0b970afadfd01cd0b38d8e7a58956e84602e2ee9
thibautpierron <tpierron@student.42.fr>
2018-04-23 21:44:49 +0200

    new sliders

--- Assets/Scripts/Stations/IStation.cs
+++ Assets/Scripts/Stations/IStation.cs
@@ -1,57 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Task))]
 abstract public class IStation : ISelectable
 {
-	public Task task;
-	public Camera cam;
+  public Task task;
+  public Camera cam;
 
-	// public Sprite[] icons;
+  private Slider slider;
 
-	public int maxWorkers = 1;
-	[SerializeField]
-	protected int currentWorkers = 0;
+  public int maxWorkers = 1;
+  [SerializeField]
+  protected int currentWorkers = 0;
 
-	// Use this for initialization
-	protected override void Start()
-	{
-		base.Start();
-		cam = Camera.main;
-		task = GetComponent<Task>();
-	}
+  protected override void Start()
+  {
+    base.Start();
+    cam = Camera.main;
+    task = GetComponent<Task>();
+    slider = GetComponentInChildren<Slider>();
+  }
 
-	protected virtual void OnGUI()
-	{
-		if (task.running)
-		{
-			var position = cam.WorldToScreenPoint(transform.position);
-			position.y = Screen.height - position.y;
-			task.DrawProgressBar(position);
-		}
-	}
+  protected override void Update()
+  {
+    base.Update();
+    if (task.running)
+    {
+      slider.value = task.completion / 100;
+    }
+  }
 
-	// returns false if station is full
-	public virtual bool AssignWorker()
-	{
-		if (currentWorkers < maxWorkers)
-		{
-			currentWorkers++;
-			OnNewWorker();
-			return true;
-		}
-		return false;
-	}
+  protected virtual void OnGUI()
+  {
+    // if (task.running)
+    // {
+    // 	var position = cam.WorldToScreenPoint(transform.position);
+    // 	position.y = Screen.height - position.y;
+    // 	task.DrawProgressBar(position);
+    // }
+  }
 
-	public virtual void OnNewWorker() { }
-	public virtual void OnLoseWorker() { }
+  // returns false if station is full
+  public virtual bool AssignWorker()
+  {
+    if (currentWorkers < maxWorkers)
+    {
+      currentWorkers++;
+      OnNewWorker();
+      return true;
+    }
+    return false;
+  }
 
-	public virtual void RemoveWorker()
-	{
-		currentWorkers -= 1;
-		if (currentWorkers < 0)
-			currentWorkers = 0;
-		OnLoseWorker();
-	}
+  public virtual void OnNewWorker() { }
+  public virtual void OnLoseWorker() { }
+
+  public virtual void RemoveWorker()
+  {
+    currentWorkers -= 1;
+    if (currentWorkers < 0)
+      currentWorkers = 0;
+    OnLoseWorker();
+  }
 }
