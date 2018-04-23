13128aaa7d8611b3c3552691a04e478eb0394747
thibautpierron <tpierron@student.42.fr>
2018-04-23 18:02:42 +0200

    tuto skeleton prepared

--- Assets/Scripts/UI/UITools.cs
+++ Assets/Scripts/UI/UITools.cs
@@ -6,29 +6,57 @@ using UnityEngine.SceneManagement;
 public class UITools : MonoBehaviour
 {
 
+  public enum eScene
+  {
+    SPLASH,
+    TUTO
+  }
+
+  public eScene scene = eScene.SPLASH;
   public Animation blackBackground;
   public GameObject text;
+
+  public GameObject[] slides;
+  private int currentSlide;
+
+  public void Start()
+  {
+    currentSlide = 0;
+    for (int i = 1; i < slides.Length; i++)
+      slides[i].SetActive(false);
+  }
 
   public void Update()
   {
     if (Input.GetKeyDown(KeyCode.Space))
     {
-      blackBackground.Play();
-      text.SetActive(false);
-      GetComponent<AudioSource>().Play();
-      StartCoroutine("WaitFadeOut");
+      if (scene == eScene.SPLASH)
+      {
+        blackBackground.Play();
+        text.SetActive(false);
+        GetComponent<AudioSource>().Play();
+        StartCoroutine("NextScene");
+      }
+      else if (scene == eScene.TUTO && currentSlide < slides.Length - 1)
+      {
+        slides[currentSlide].SetActive(false);
+        currentSlide++;
+        slides[currentSlide].SetActive(true);
+      }
+      else
+      {
+        blackBackground.Play();
+        text.SetActive(false);
+        // GetComponent<AudioSource>().Play();
+        StartCoroutine("NextScene");
+      }
     }
 
   }
-  public void NextScene()
-  {
-    // Debug.Log("Next Scene");
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-  }
 
-  IEnumerator WaitFadeOut()
+  IEnumerator NextScene()
   {
     yield return new WaitForSeconds(2);
-    NextScene();
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
   }
 }
