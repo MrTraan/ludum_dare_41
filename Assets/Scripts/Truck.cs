64cba88447ad5c097c592c4d8e7a8c05c9653338
thibautpierron <tpierron@student.42.fr>
2018-04-24 01:47:13 +0200

    add truck sounds

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -5,85 +5,92 @@ using UnityEngine;
 public class Truck : IStation
 {
 
-	public enum eState
-	{
-		TRAVEL,
-		UNLOADING,
-		EMPTY,
-	}
+  public enum eState
+  {
+    TRAVEL,
+    UNLOADING,
+    EMPTY,
+  }
 
-	public eState state = eState.EMPTY;
-	public float travelTime = 30;
+  public eState state = eState.EMPTY;
+  public float travelTime = 30;
 
-	private Animator animator;
-	private Dictionary<eResource, int> stock = new Dictionary<eResource, int>();
+  public AudioClip startSound;
+  public AudioClip klaxonSound;
 
-	protected override void Start()
-	{
-		base.Start();
-		animator = GetComponentInChildren<Animator>();
-		animator.SetTrigger("Arrive");
-	}
+  private Animator animator;
+  private Dictionary<eResource, int> stock = new Dictionary<eResource, int>();
 
-	protected override void Update()
-	{
-		base.Update();
+  protected override void Start()
+  {
+    base.Start();
+    animator = GetComponentInChildren<Animator>();
+    animator.SetTrigger("Arrive");
+  }
 
-
-		if (state == eState.UNLOADING && task.IsCompleted())
-		{
-			foreach (var command in stock)
-				GameManager.resourceManager.Add(command.Key, command.Value);
-			task.Reset();
-			state = eState.EMPTY;
-			Reset();
-		}
-		else if (state == eState.UNLOADING)
-		{
-			task.Progress(currentWorkers * 1.0f);
-		}
-	}
-
-	public void GoShopping(Dictionary<eResource, int> shoppingList)
-	{
-		stock = new Dictionary<eResource, int>(shoppingList);
-		StartCoroutine("Travel");
-		animator = GetComponentInChildren<Animator>();
-	}
-
-	public override TaskLayout GetTaskLayout()
-	{
-		TaskLayout layout = new TaskLayout();
-		layout.type = eTaskLayoutType.TRUCK_ORDER;
-		layout.truckOrder = stock;
-
-		return layout;
-	}
+  protected override void Update()
+  {
+    base.Update();
 
 
-	IEnumerator Travel()
-	{
-		animator.SetTrigger("Leave");
-		state = eState.TRAVEL;
+    if (state == eState.UNLOADING && task.IsCompleted())
+    {
+      foreach (var command in stock)
+        GameManager.resourceManager.Add(command.Key, command.Value);
+      task.Reset();
+      state = eState.EMPTY;
+      Reset();
+    }
+    else if (state == eState.UNLOADING)
+    {
+      task.Progress(currentWorkers * 1.0f);
+    }
+  }
 
-		yield return new WaitForSeconds(travelTime);
+  public void GoShopping(Dictionary<eResource, int> shoppingList)
+  {
+    stock = new Dictionary<eResource, int>(shoppingList);
+    StartCoroutine("Travel");
+    animator = GetComponentInChildren<Animator>();
+  }
 
-		state = eState.UNLOADING;
-		animator.SetTrigger("Arrive");
-		task.Begin();
-	}
+  public override TaskLayout GetTaskLayout()
+  {
+    TaskLayout layout = new TaskLayout();
+    layout.type = eTaskLayoutType.TRUCK_ORDER;
+    layout.truckOrder = stock;
 
-	public override void HandleOrder(Order o)
-	{
-	}
+    return layout;
+  }
 
-	public void Reset()
-	{
-		stock[eResource.FISH] = 0;
-		stock[eResource.STEAK] = 0;
-		stock[eResource.CHICKEN] = 0;
-		stock[eResource.PEAS] = 0;
-		stock[eResource.CAROT] = 0;
-		stock[eResource.POTATOES] = 0;
-	}
+
+  IEnumerator Travel()
+  {
+    animator.SetTrigger("Leave");
+    state = eState.TRAVEL;
+    GetComponent<AudioSource>().clip = startSound;
+    GetComponent<AudioSource>().Play();
+
+    yield return new WaitForSeconds(travelTime);
+
+    GetComponent<AudioSource>().clip = klaxonSound;
+    GetComponent<AudioSource>().Play();
+    state = eState.UNLOADING;
+    animator.SetTrigger("Arrive");
+    task.Begin();
+  }
+
+  public override void HandleOrder(Order o)
+  {
+  }
+
+  public void Reset()
+  {
+    stock[eResource.FISH] = 0;
+    stock[eResource.STEAK] = 0;
+    stock[eResource.CHICKEN] = 0;
+    stock[eResource.PEAS] = 0;
+    stock[eResource.CAROT] = 0;
+    stock[eResource.POTATOES] = 0;
+  }
 }
